26a6d0d3b7455800e61057b170c1231f9665f8a8
Alberto Rigoni <albertorigoni200@gmail.com>
2025-01-31 23:02:48 +0100

    delete button done, only read button missing now

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
     let search = document.querySelector('#search');
     let library = document.querySelector('#library');
     let saver = document.querySelector('#saver');
-
+ 
     function Book( title_value, author_value, pages_value, read_value) {
         this.title = title_value;
         this.author = author_value;
@@ -17,9 +17,12 @@ document.addEventListener('DOMContentLoaded', () => {
 
     function renderBooks() {
         library.innerHTML = '';
+
         for (let book of books_array){
+
+            let book_id = book.title.replaceAll(' ', '-');
             library.innerHTML +=  `
-            <div class="book">
+            <div class="book" id='${book_id}'>
                 <div class="title-area">
                     <h2>${book.title}</h2>
                 </div>
@@ -31,11 +34,22 @@ document.addEventListener('DOMContentLoaded', () => {
                 </div>
                 <div class="icons">
                     <img src="bookmark_24dp_FFFFFF_FILL0_wght400_GRAD0_opsz24.png" class="icon">
-                    <img src="delete_24dp_FFFFFF_FILL0_wght400_GRAD0_opsz24.png" class="icon">
+                    <img src="delete_24dp_FFFFFF_FILL0_wght400_GRAD0_opsz24.png" class="icon delete-icon">
             </div>
              `;
         }
+        let deleteIcons = Array.from(document.querySelectorAll('.delete-icon'));
+
+        for(let delete_icon of deleteIcons) {
+            delete_icon.addEventListener('click', () => {
+                books_array = books_array.filter((b) => {
+                    b.title.replaceAll(' ', '-') != delete_icon.parentElement.id;
+                })
+                renderBooks();
+            })
+        }
     }
+        
 
     saver.addEventListener('click', (e) => {
         e.preventDefault();
